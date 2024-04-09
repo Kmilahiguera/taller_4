a0fc84f01573e5dece019e831f934d49ef914a0f
kmila <camila15011@gmail.com>
2024-04-09 15:38:53 -0500

    funciones

--- taller.py
+++ taller.py
@@ -2,3 +2,14 @@ import numpy as np
 
 miLista=[3 , 5 ,7,9]
 
+miArreglo= np.array(miLista)
+
+print(miArreglo.ndim)
+print(miArreglo.shape)
+print(miArreglo.size)
+print(miArreglo.dtype)
+
+miArreglo2=np.array([3, 6, 7, 90])
+
+miArreglo=miArreglo=np.arange(-10, 10)
+print(miArreglo)
